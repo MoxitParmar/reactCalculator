cef6bf515d6751930f48f08687468fa670f3584b
moxitparmar <moxitparmar2004@gmail.com>
2024-07-08 23:59:44 +0530

    minor changes

--- src/Components/Screen.jsx
+++ src/Components/Screen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Screen = ({screen}) => {
   return (
-      <div className='bg-zinc-300 h-[25%] rounded-t-2xl flex justify-end items-end pb-10 pr-10'>
+      <div className='bg-zinc-300 h-[10vh] sm:h-[12vh] rounded-t-2xl flex justify-end items-end pb-10 pr-10'>
       <span className='text-blue-950 text-5xl font-bold'>{ screen }</span>
       </div>
   )
